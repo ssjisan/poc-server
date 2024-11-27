7f3c446e699ff8bf31380fc3bb7131438157a34b
ssjisan <ssjisan.dev@gmail.com>
2024-11-28 01:57:44 +0600

    appointment

--- controller/appointmentController.js
+++ controller/appointmentController.js
@@ -93,11 +93,7 @@ export const submitAppointment = async (req, res) => {
               <tr>
                 <td><strong>Date:</strong></td>
                 <td style="color: #f44336; font-size: 18px;">
-                  ${new Date(appointmentDate).toLocaleDateString("en-US", {
-                  day: "2-digit",
-                  month: "short",
-                  year: "numeric",
-                })}
+                  ${appointmentDate}
                 </td>
               </tr>
             </table>
